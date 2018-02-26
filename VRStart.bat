3282040ba22eb58669fc4475ae45841f35d5bd3a
Warrick <warrickctjunk@gmail.com>
2018-02-26 15:25:20 +1300

    Adding .bat file.

---
+++ VRStart.bat
@@ -0,0 +1,30 @@
+echo checking internet connection
+ping 8.8.8.8 -n 1 -w 1000
+cls
+if errorlevel 1 (
+	set internet=false
+) else (
+	set internet=true
+)
+
+echo internet connected: %internet%
+
+cd C:\"Program Files (x86)\Steam\config"
+if %internet%==true (
+	ren "loginusers.vdf" "loginusers - offline.vdf"
+	ren "loginusers - online.vdf" "loginusers.vdf"
+)
+
+start C:\"Program Files (x86)\Steam\Steam.exe"
+
+timeout /t 20 /nobreak
+
+if %internet%==true (
+	ren "loginusers.vdf" "loginusers - online.vdf"
+	ren "loginusers - offline.vdf" "loginusers.vdf"
+)
+
+REM start C:\"Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrstartup.exe"
+start C:"\Program Files (x86)\Steam\steamapps\common\SteamVR\bin\win64\vrmonitor.exe"
+
+pause
